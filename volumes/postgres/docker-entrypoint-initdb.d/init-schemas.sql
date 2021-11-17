5d182439560e07e09ddfed7248c9e4536d6d6bb1
timbset <timbset@gmail.com>
2021-11-17 17:29:32 +0300

    Create new user in postgres

--- volumes/postgres/docker-entrypoint-initdb.d/init-schemas.sql
+++ volumes/postgres/docker-entrypoint-initdb.d/init-schemas.sql
@@ -1,17 +1,20 @@
 \c postgres;
 
+CREATE USER "hn-user";
+ALTER USER "hn-user" PASSWORD 'QweZxc123';
+
 CREATE SCHEMA "event-store";
-GRANT USAGE ON SCHEMA "event-store" TO "hn-admin";
-GRANT ALL ON SCHEMA "event-store" TO "hn-admin";
-GRANT ALL ON ALL TABLES IN SCHEMA "event-store" TO "hn-admin";
-GRANT ALL ON ALL SEQUENCES IN SCHEMA "event-store" TO "hn-admin";
-GRANT ALL ON ALL FUNCTIONS IN SCHEMA "event-store" TO "hn-admin";
-ALTER SCHEMA "event-store" OWNER TO "hn-admin";
+GRANT USAGE ON SCHEMA "event-store" TO "hn-user";
+GRANT ALL ON SCHEMA "event-store" TO "hn-user";
+GRANT ALL ON ALL TABLES IN SCHEMA "event-store" TO "hn-user";
+GRANT ALL ON ALL SEQUENCES IN SCHEMA "event-store" TO "hn-user";
+GRANT ALL ON ALL FUNCTIONS IN SCHEMA "event-store" TO "hn-user";
+ALTER SCHEMA "event-store" OWNER TO "hn-user";
 
 CREATE SCHEMA "read-store";
-GRANT USAGE ON SCHEMA "read-store" TO "hn-admin";
-GRANT ALL ON SCHEMA "read-store" TO "hn-admin";
-GRANT ALL ON ALL TABLES IN SCHEMA "read-store" TO "hn-admin";
-GRANT ALL ON ALL SEQUENCES IN SCHEMA "read-store" TO "hn-admin";
-GRANT ALL ON ALL FUNCTIONS IN SCHEMA "read-store" TO "hn-admin";
-ALTER SCHEMA "read-store" OWNER TO "hn-admin";
+GRANT USAGE ON SCHEMA "read-store" TO "hn-user";
+GRANT ALL ON SCHEMA "read-store" TO "hn-user";
+GRANT ALL ON ALL TABLES IN SCHEMA "read-store" TO "hn-user";
+GRANT ALL ON ALL SEQUENCES IN SCHEMA "read-store" TO "hn-user";
+GRANT ALL ON ALL FUNCTIONS IN SCHEMA "read-store" TO "hn-user";
+ALTER SCHEMA "read-store" OWNER TO "hn-user";
